2e4dab9c17d1ec109cff98539a022016ed5da8a9
Saif-Uddin063 <mahirrahman063@gmail.com>
2025-10-28 16:22:09 +0600

    implement the function add data and show it to ui

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 
 app.get('/',(req ,res)=>{
@@ -19,6 +20,16 @@ const users = [
 
 app.get('/users' , (req, res)=>{
     res.send(users);
+})
+
+app.post('/users',(req,res)=>{
+    console.log('post method call',req.body);
+    const newUser = req.body;
+    newUser.id = users.length+1;
+    users.push(newUser);
+    res.send(newUser);
+    
+    
 })
 
 
